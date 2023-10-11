e29ea24b82dec3b2e8599b25fd5ee55a237c55ff
matheusvidal21 <matheusvidal140@gmail.com>
2023-10-11 11:10:23 -0300

    Initial commit

---
+++ src/main/java/br/com/matheusvidal/todolist/task/TasksController.java
@@ -0,0 +1,22 @@
+package br.com.matheusvidal.todolist.task;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tasks")
+public class TasksController {
+
+    @Autowired
+    private ITaskRepository taskRepository;
+
+    @PostMapping("/")
+    public TaskModel create(@RequestBody TaskModel taskModel){
+
+        var task = this.taskRepository.save(taskModel);
+        return task;
+    }
+}
